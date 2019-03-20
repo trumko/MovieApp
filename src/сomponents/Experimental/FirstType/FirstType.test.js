7a8f7d4beb2c6e55088d64fdf94f4761a8eafaa2
Mykhailo Yartysh <miartysh@cartera.com>
2019-03-20 17:16:38 +0200

    fixed tests

--- src/сomponents/Experimental/FirstType/FirstType.test.js
+++ src/сomponents/Experimental/FirstType/FirstType.test.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
+import { shallow } from 'enzyme';
 import FirstType from './FirstType';
 
-configure({adapter: new Adapter()});
-
 describe('FirstType', () => {
-  it('should render correctly', () => {
+  it('renders component with "First" title', () => {
     const output = shallow(
       <FirstType>First title</FirstType>
     );
-    expect(shallowToJson(output)).toMatchSnapshot();
+    expect(output).toMatchSnapshot();
   });
-  it('should render correctly', () => {
+  it('renders component with "Second" title', () => {
     const output = shallow(
       <FirstType>Second title</FirstType>
     );
-    expect(shallowToJson(output)).toMatchSnapshot();
+    expect(output).toMatchSnapshot();
   });
 });

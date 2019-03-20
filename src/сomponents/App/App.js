65dfe0347856ec3c4ae6e9902417a4f8e40d4005
Mykhailo Yartysh <miartysh@cartera.com>
2019-03-20 15:26:41 +0200

    fixed dependecies and cleaned App component

--- src/сomponents/App/App.js
+++ src/сomponents/App/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import FirstType from "../Experimental/FirstType/FirstType";
 import { SecondType } from "../Experimental/SecondType";
 import ThirdType from "../Experimental/ThirdType";
 import FourthType from "../Experimental/FourthType";
 
 export default class App extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <div>

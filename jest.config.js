259105171989a42e7932909d3d0a0f6ec148764f
trumko <trumko@gmail.com>
2019-04-15 00:59:57 +0300

    Added basic test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "identity-obj-proxy"
+    "\\.(css|scss|less)$": "identity-obj-proxy"
   },
   snapshotSerializers: [
     "enzyme-to-json/serializer"
   ],
-  setupFiles: ['<rootDir>/enzyme.config.js']
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+  moduleDirectories: [
+    "node_modules",
+    "src"
+  ]
 };
--- src/components/AppWrapper/AppWrapper.js
+++ src/components/AppWrapper/AppWrapper.js
@@ -18,6 +18,7 @@ export default function AppWrapper() {
           <Page />
           <Footer />
         </Router>
+        <p>This is test line</p>
       </ErrorBoundary>
     </Provider>
   );

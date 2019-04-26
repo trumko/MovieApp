5a4bab13b3c41935d55fed950fbe155085c4ba70
trumko <trumko@gmail.com>
2019-04-27 00:46:38 +0300

    Implemented Offline data storage and store restoration

--- src/components/AppWrapper/AppWrapper.js
+++ src/components/AppWrapper/AppWrapper.js
@@ -2,8 +2,9 @@ import React from "react";
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from "react-redux";
-import store from "store/index";
+import { persistor, store } from "store/index";
 
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary'
 import Page from 'components/Pages/Page';
 import Footer from 'components/Footer/Footer';
@@ -13,12 +14,14 @@ import './AppWrapper.scss'
 export default function AppWrapper() {
   return (
     <Provider store={store}>
-      <ErrorBoundary>
-        <Router>
-          <Page />
-          <Footer />
-        </Router>
-      </ErrorBoundary>
+      <PersistGate loading={null} persistor={persistor}>
+        <ErrorBoundary>
+          <Router>
+            <Page />
+            <Footer />
+          </Router>
+        </ErrorBoundary>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,29 @@
 import { createStore, compose, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
 import createSagaMiddleware from 'redux-saga'
 
 import { rootReducer } from "../reducers/index";
 import rootSaga from '../sagas';
 
+const persistConfig = {
+ key: 'root',
+ storage: storage,
+ stateReconciler: autoMergeLevel2
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer,
+export const store = createStore(
+  pReducer,
   composeEnhancer(applyMiddleware(sagaMiddleware))
 );
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga)
 
